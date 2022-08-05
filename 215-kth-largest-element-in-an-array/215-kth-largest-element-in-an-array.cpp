e8c61dd0cd5dea68ce3fa888a47700c25edcbb93
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-08-05 22:09:00 +0530

    Time: 167 ms (13.51%), Space: 47.3 MB (7.68%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,19 @@
+//STRIVER_SDE_SHEET
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& a, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0; i<k; i++){
+            pq.push(a[i]);
+        }
+        for(int i=k; i<a.size(); i++){
+            if(a[i]>pq.top()){
+                pq.pop();
+                pq.push(a[i]);
+            }
+        }
+        return pq.top();
+        
+    }
+};
